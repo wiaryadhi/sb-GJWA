77b9b64968bdbf01828c239069c5649932280cf9
ARY <widhiarya@outlook.com>
2022-11-08 10:21:44 +0700

    update code template untuk new java class

--- fundamentalmaven/src/main/java/com/bcafinance/training/day2/firstLesson.java
+++ fundamentalmaven/src/main/java/com/bcafinance/training/day2/firstLesson.java
@@ -1,5 +1,18 @@
 package com.bcafinance.training.day2;
 
+/*
+@Author wiary a.k.a. Arya
+ITDP 7
+------------------------------------
+| Created with:                    |
+| Eclipse Version: 2022-09 (4.25.0)|
+| Build id: 20220908-1902          |
+------------------------------------
+Created on 08-11-2022 10:21:04
+Last Modified on 08-11-2022 10:21:04
+Version 1.0
+*/
+
 public class firstLesson {
 
 }
