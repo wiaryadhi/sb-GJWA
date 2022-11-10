9547a8cfbf0c6bef56169e8fd1d027fe6517c5b1
ARY <widhiarya@outlook.com>
2022-11-10 14:36:29 +0700

    commit soal 1 sudah selesai dengan optimize

---
+++ JF-GJWA/src/main/java/com/bcafinance/fundamental/day4/Soal2.java
@@ -0,0 +1,16 @@
+package com.bcafinance.fundamental.day4;
+/*
+@Author wiary a.k.a. Arya
+ITDP 7
+---------------------------------------
+| Created with:IntelliJ IDEA          |
+| Version: 2022.2.3 (Ultimate Edition)|
+| Build id: #IU-222.4345.14           |
+---------------------------------------
+Created on 10/11/2022 - 14:36
+Last Modified on 10/11/2022 - 14:36
+Version 1.0
+*/
+
+public class Soal2 {
+}
