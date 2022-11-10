1602ad8799cab4455f7a9c74accea0839c5c84a9
ARY <widhiarya@outlook.com>
2022-11-10 15:29:02 +0700

    commit soal 1 sudah selesai dengan optimize

--- JF-GJWA/src/main/java/com/bcafinance/fundamental/day4/Soal2.java
+++ JF-GJWA/src/main/java/com/bcafinance/fundamental/day4/Soal2.java
@@ -13,4 +13,32 @@ Version 1.0
 */
 
 public class Soal2 {
-}
+    public static void main(String[] args) {
+        int keyword = 100;
+        int[] val = {66, 77, 80, 84, 88, 99, 100};
+        int low = 0;
+        int high = val.length - 1;
+        int med = 0;
+        boolean status = false;
+
+
+        while (low <= high) {
+            med = (low + high) / 2;
+            if (keyword==val[med]){
+                System.out.println(keyword + " FOUNDED in position " + (med+1) + " and INDEX " + med);
+                status = true;
+                break;
+            } else {
+                if (keyword>val[med]){
+                    low = med+1;
+                } else {
+                    high = med-1;
+                }
+            }
+        }  if (status==false) {
+            System.out.println(keyword + " NOT FOUND!!");
+        }
+
+
+        }
+    }
